3da8ddb0089c9b53794fe6c8d113d93cb77de268
Navin Sharma <navinsharmasuprb@gmail.com>
2020-12-06 11:35:35 +0530

    Table added

---
+++ src/component/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Navbar} from 'react-bootstrap'
+
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar bg="light">
+                <Navbar.Brand href="#home">Primer League</Navbar.Brand>
+            </Navbar>
+
+        </div>
+    )
+}
+
+export default Header
